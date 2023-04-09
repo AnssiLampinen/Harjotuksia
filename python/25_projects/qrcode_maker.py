f015d95bdc963b99b61d5422e075acf538289ba1
Anssi Lampinen <anssi.lampinen@gmail.com>
2023-04-10 01:00:11 +0300

    QRcode maker

--- python/25_projects/qrcode_maker.py
+++ python/25_projects/qrcode_maker.py
@@ -1,7 +1,7 @@
 import qrcode
 
-data = "REEEEEEEEEEEEE!"
+data = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
 img = qrcode.make(data)
 
-img.save('C:/Users/anssi/Desktop/Testing/test.png')
+img.save('C:/Users/anssi/Desktop/Testing/rick_roll.png')
