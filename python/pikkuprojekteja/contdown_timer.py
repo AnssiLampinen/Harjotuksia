52988cd8ffaf2f2d3371fd2c436ab27fea547bfc
Anssi Lampinen <anssi.lampinen@gmail.com>
2023-04-09 22:48:24 +0300

    Create contdown_timer.py

---
+++ python/pikkuprojekteja/contdown_timer.py
@@ -0,0 +1,16 @@
+import time
+            
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end = "\r")
+        time.sleep(1)
+        t -= 1
+    print("Time's up!")
+
+def main():
+    t = input("How many seconds you want to count down from? ")
+    countdown(int(t))
+
+main()
