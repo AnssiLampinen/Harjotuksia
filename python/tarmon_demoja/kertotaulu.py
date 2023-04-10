7dd891740c2b97197961f91549cf42fc09edf564
Anssi Lampinen <anssi.lampinen@gmail.com>
2023-04-10 13:00:09 +0300

    Update kertotaulu.py

--- python/tarmon_demoja/kertotaulu.py
+++ python/tarmon_demoja/kertotaulu.py
@@ -1,5 +1,5 @@
 def kertotaulu():
-    user_input = int(input("Choose a number"))
+    user_input = int(input("Choose a number: "))
     for i in range(11):
         print("{} x {} = {}". format(i, user_input, i * user_input))
 
